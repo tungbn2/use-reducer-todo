77b0fa136ea8205d660bf1565907bd9276d937df
tungbn2 <tung.ftu.06@gmail.com>
2021-12-29 09:05:36 +0700

    react-todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer, useRef, useEffect } from "react";
+
+// init state
+const initState = {
+  job: "",
+  jobs: [],
+};
+
+// action
+const SET_JOB = "set_job";
+const ADD_JOB = "add_job";
+const DELETE_JOB = "delete_job";
+
+const setJob = (payload) => ({ type: SET_JOB, payload });
+const addJob = (payload) => ({ type: ADD_JOB, payload });
+const deleteJob = (payload) => ({ type: DELETE_JOB, payload });
+
+// reducer
+const reducer = (initState, action) => {
+  let newState = initState;
+
+  switch (action.type) {
+    case SET_JOB:
+      newState = { ...initState, job: action.payload };
+      break;
+
+    case ADD_JOB:
+      newState = { ...initState, jobs: [...initState.jobs, action.payload] };
+      break;
+
+    case DELETE_JOB:
+      let newJobs = initState.jobs.filter(
+        (job, index) => index !== action.payload
+      );
+      newState = { ...initState, jobs: newJobs };
+      break;
+
+    default:
+      throw new Error("invalid action");
+  }
+
+  console.log(newState);
+  return newState;
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initState);
+  const { job, jobs } = state;
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: "2rem" }}>
+      <h2>Todo</h2>
+      <input
+        ref={inputRef}
+        value={job}
+        placeholder="Add todo ..."
+        onChange={(e) => dispatch(setJob(e.target.value))}
+      />
+      <button
+        onClick={() => {
+          dispatch(addJob(job));
+          dispatch(setJob(""));
+          inputRef.current.focus();
+        }}
+      >
+        Add
+      </button>
+      <hr />
+      <ul>
+        {jobs.map((jobName, index) => (
+          <li key={index}>
+            {jobName} -{" "}
+            <span onClick={() => dispatch(deleteJob(index))}>&times;</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
